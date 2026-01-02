f02b01d1ed38bde8a73b92767f9821829b896184
KyleJuris <kylerjuris@gmail.com>
2026-01-02 06:12:26 +0400

    Add Google site verification meta tag

--- app/_layout.web.tsx
+++ app/_layout.web.tsx
@@ -1,25 +1,31 @@
 import React from "react"
 import { Stack } from "expo-router"
+import { Head } from "expo-router/head"
 import { AuthProvider } from "../context/AuthContext"
 import { LibraryProvider } from "../context/LibraryContext"
 import { ThemeProvider } from "../context/ThemeContext"
 
 export default function RootLayout() {
   return (
-    <ThemeProvider>
-      <AuthProvider>
-        <LibraryProvider>
-          <Stack
-            screenOptions={{
-              headerShown: false,
-            }}
-          >
-            <Stack.Screen name="(tabs)" />
-            <Stack.Screen name="reader" />
-          </Stack>
-        </LibraryProvider>
-      </AuthProvider>
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta name="google-site-verification" content="f4hE6CjKLJ6awC43ORfLGx9m34-MYJy13FZ3qqSpNw8" />
+      </Head>
+      <ThemeProvider>
+        <AuthProvider>
+          <LibraryProvider>
+            <Stack
+              screenOptions={{
+                headerShown: false,
+              }}
+            >
+              <Stack.Screen name="(tabs)" />
+              <Stack.Screen name="reader" />
+            </Stack>
+          </LibraryProvider>
+        </AuthProvider>
+      </ThemeProvider>
+    </>
   )
 }
 
