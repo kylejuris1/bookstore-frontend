64ad763dcf59de21db769aaf56c65f703bf14597
KyleJuris <kylerjuris@gmail.com>
2026-01-05 15:23:25 +0400

    Fix React error #130 by removing Head component that is undefined in production build

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,6 @@
-import { Head } from "expo-router/head";
 import HomeScreen from "../../screens/HomeScreen";
 
 export default function Index() {
-  return (
-    <>
-      <Head>
-        <meta name="google-site-verification" content="f4hE6CjKLJ6awC43ORfLGx9m34-MYJy13FZ3qqSpNw8" />
-      </Head>
-      <HomeScreen />
-    </>
-  );
+  return <HomeScreen />;
 }
 
